acf15330c570f45e72741e414f1ef70445ade4d0
samik shk <samik.shakya8848@gmail.com>
2023-02-13 18:50:39 +0545

    first assignment

---
+++ Samik.java
@@ -0,0 +1,16 @@
+public class Samik{
+    public static void main(String[] args)
+    {
+        System.out.println("Name of the organization: Kamalpokhari Restaurant & Bar");
+        System.out.println("\n");
+        System.out.println("Address:");
+        System.out.println("----State: KM");
+        System.out.println("----Zip Code: 44600");
+        System.out.println("----Country: Nepal");
+        System.out.println("\n");
+        System.out.println("Contact Information:");
+        System.out.println("----Mobile Number: 9865382409");
+        System.out.println("----Email: kamalpokhari.rnb@gmail.com");
+
+    }
+}
